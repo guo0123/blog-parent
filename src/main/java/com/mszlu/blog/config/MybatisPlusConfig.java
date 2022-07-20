3f66a35ff6ba0f41a7dcf41fc1ae58ffcbcd66c0
guoyanhong <3260926252@qq.com>
2022-07-20 16:04:15 +0800

    test

---
+++ src/main/java/com/mszlu/blog/config/MybatisPlusConfig.java
@@ -0,0 +1,24 @@
+package com.mszlu.blog.config;
+
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@MapperScan("com.mszlu.blog.dao.mapper")
+public class MybatisPlusConfig {
+
+
+    //分页插件
+    //@Configuration可理解为用spring的时候xml里面的<beans>标签。
+    // @Bean可理解为用spring的时候xml里面的<bean>标签。
+    @Bean
+    public MybatisPlusInterceptor mybatisPlusInterceptor(){
+        MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
+        interceptor.addInnerInterceptor(new PaginationInnerInterceptor());
+        return interceptor;
+    }
+
+}
---
+++ src/main/java/com/mszlu/blog/config/WebConfig.java
@@ -0,0 +1,15 @@
+package com.mszlu.blog.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        //跨域配置,不可设置为*,不安全,前后端分离项目,可能域名不一致
+        //本地测试,端口不一致,也算跨域
+        registry.addMapping("/**").allowedOrigins("http://localhost:8080");
+    }
+}
